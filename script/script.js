a8d77e4a05c779b0e9ef9a4c721c6534ad7f984c
KovalovIE <kovalevie1@gmail.com>
2018-11-08 04:07:27 +0200

    create-read

--- script/script.js
+++ script/script.js
@@ -1,74 +1,3 @@
-// var db;
-// var request = indexedDB.open("DatabasePerson", 3);
-//
-// request.onerror = function(event) {
-//     // Сделать что-то при ошибке request.errorCode!
-// };
-// request.onupgradeneeded  = function(event) {
-//     db = event.target.result;
-//     console.log(db)
-//
-//     var objectStore = db.createObjectStore("people", {autoIncrement:true});
-// };
-
-// This is what our customer data looks like.
-
-// var db;
-//
-// let newPerson = {};
-//
-// let customerData = [
-//     { id: "1", Fname: "FDhhdf", Lname: 'BBB', age: 10 },
-//     { id: "2", Fname: "WQWR", Lname: 'CCC', age: 02 }
-// ];
-//
-// const createPerson = document.getElementById('createPerson');
-//
-//
-// const dbName = "dbPerson";
-// var request = indexedDB.open(dbName, 2);
-// //console.log('1111')
-// request.onerror = function(event) {
-//     alert('ERROR')
-// };
-// request.onupgradeneeded = function(event) {
-//     console.log('aaaaaa')
-//     db = event.target.result;
-//     console.log(db)
-//     var objectStore = db.createObjectStore("people", {autoIncrement:true});
-//     // objectStore.createIndex("Fname", "Fname", { unique: false });
-//     // objectStore.createIndex("Lname", "Lname", { unique: true });
-//     // objectStore.createIndex("age", "age", { unique: false });
-//     // for (var i in customerData) {
-//     //     objectStore.add(customerData[i]);
-//     // }
-// };
-// createPerson.addEventListener('click', addNewPerson);
-// console.log(db)
-// function addNewPerson() {
-//
-//     newPerson.id = document.getElementById('addId').value;
-//     newPerson.Fname = document.getElementById('addFname').value;
-//     newPerson.Lname = document.getElementById('addLname').value;
-//     newPerson.age = document.getElementById('addAge').value;
-//
-//     customerData.push(newPerson)
-//     console.log(customerData)
-//
-//     var trans = db.transaction(["persons"], "readwrite");
-//     var store = trans.objectStore("persons");
-//
-//     // var data = {
-//     //     "text": todoText,
-//     //     "timeStamp": new Date().getTime()
-//     // };
-//
-//     var request = store.put(newPerson,2);
-// }
-
-// function indexedDBOk() {
-//     return "indexedDB" in window;
-// }
 document.addEventListener("DOMContentLoaded", function() {
     
     var openRequest = indexedDB.open("personFile",2);
@@ -116,9 +45,7 @@ getPeople1.addEventListener('click', getPeople)
         db.transaction(["people"], "readonly").objectStore("people").openCursor().onsuccess = function(e) {
             var cursor = e.target.result;
             
-            var searchId = document.getElementById('addId').value;
-
-            
+            var searchId = document.getElementById('addId').value;            
                 
                 if(cursor) {
                     for(var field in cursor.value) {
@@ -145,7 +72,7 @@ getPeople1.addEventListener('click', getPeople)
 
                             arrDataPersons.push(cursor.value[field])
                             console.log(arrDataPersons)
-                            
+
                             td1.innerHTML = cursor.value.id;
                             td2.innerHTML = cursor.value.id;
                             td3.innerHTML = cursor.value.id;
@@ -156,10 +83,6 @@ getPeople1.addEventListener('click', getPeople)
                     }
                     cursor.continue();
                 }
-                //console.log(arrDataPersons)
-                
-
-                        
         }
     }
 }
